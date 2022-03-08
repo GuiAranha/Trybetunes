50b340515c053b6a54c7c87e7e40d7e993920665
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-03-08 15:22:20 -0300

    Requisito 5 - ok

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,50 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      btnDisabled: true,
+      nameArtist: '',
+    };
+    this.enableButtonSearch = this.enableButtonSearch.bind(this);
+  }
+
+  enableButtonSearch(event) {
+    this.setState({ nameArtist: event.target.value });
+    const min = 2;
+    if (event.target.value.length >= min) {
+      this.setState({ btnDisabled: false });
+    }
+  }
+
   render() {
+    const { btnDisabled, nameArtist } = this.state;
+
     return (
       <div data-testid="page-search">
         <p>Search</p>
         <Header />
+        <form>
+          <label htmlFor="name">
+            Nome do artista:
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              id="name"
+              value={ nameArtist }
+              onChange={ this.enableButtonSearch }
+            />
+          </label>
+
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ btnDisabled }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
